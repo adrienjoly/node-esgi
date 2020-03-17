ab7448ca7f4ad2b39f577d77211b1222100d62e2
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-17 17:05:47 +0100

    use Promise with readFile()

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const util = require('util')
 const express = require('express')
 const app = express()
 
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
 app.use(express.json()) // for parsing application/json
 
 const PORT = process.env.PORT || 3000;
@@ -28,27 +31,12 @@ app.post('/chat', function (req, res) {
   } else {
     if (/ = /.test(req.body.msg)) {
       const [ cle, valeur ] = req.body.msg.split(' = ')
-      // const valeursExistantes = readValuesFromFile();
-      readValuesFromFile((err, valeursExistantes) => {
-        if (err) {
-          res.send('error while reading réponses.json', err)
-        } else {
+      readValuesFromFile()
+        .then(valeursExistantes => {
           const data = JSON.stringify({
             ...valeursExistantes,
             [cle]: valeur
           })
-          /*
-          fs.writeFile('réponses.json', data, (err) => {
-            console.log('appel au callback de writefile')
-            if (err) {
-              console.error('error while saving réponses.json', err)
-              res.send('Il y a eu une erreur lors de l\'enregistrement')
-            } else {
-              res.send('Merci pour cette information !')
-            }
-          });
-          */
-          const writeFile = util.promisify(fs.writeFile);
           writeFile('réponses.json', data).then(() => {
             console.log('appel au callback de writefile')
             res.send('Merci pour cette information !')
@@ -58,20 +46,20 @@ app.post('/chat', function (req, res) {
             res.send('Il y a eu une erreur lors de l\'enregistrement')
           });
           console.log('appel à writefile effectué')
-        }
-      })
-
-
+        })
+        .catch(err => {
+          res.send('error while reading réponses.json', err)
+        })
     } else {
       const cle = req.body.msg
-      readValuesFromFile((err, values) => {
-        if (err) {
-          res.send('error while reading réponses.json', err)
-        } else {
+      readValuesFromFile()
+        .then((values) => {
           const reponse = values[cle]
           res.send(cle + ': ' + reponse)
-        }
-      })
+        })
+        .catch((err) => {
+          res.send('error while reading réponses.json', err)
+        })
     }
   }
 })
@@ -80,6 +68,7 @@ app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
 })
 
+/*
 function readValuesFromFile(callback) {
   fs.readFile('réponses.json', { encoding: 'utf8' }, (err, reponses) => {
     if (err) {
@@ -89,5 +78,19 @@ function readValuesFromFile(callback) {
       callback(null, valeursExistantes);
     }
   });
+}
+*/
+
+function readValuesFromFile() {
+  return new Promise((resolve, reject) => {
+    readFile('réponses.json', { encoding: 'utf8' })
+      .then(reponses => {
+        const valeursExistantes = JSON.parse(reponses);
+        resolve(valeursExistantes);
+      })
+      .catch(err => {
+        reject(err);
+      })
+  })
 }
 
