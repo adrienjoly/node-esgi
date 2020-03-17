472c5895ac7313bba1ed603ff5e0263ca05b0e30
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-17 10:26:35 +0100

    add endpoint GET /hello

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/hello', function (req, res) {
+  const nom = req.query.nom
+  if (nom) {
+    res.send('Bonjour, ' + nom + ' !')
+  } else {
+    res.send('Quel est votre nom ?')
+  }
+})
+
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
 })
