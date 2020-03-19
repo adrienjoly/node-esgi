e2d4cb309bfffcd930ba16e761b386e4864a3711
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-19 10:14:51 +0100

    use local mongodb docker image

--- dates.js
+++ dates.js
@@ -4,12 +4,15 @@ const PASSWORD = process.env.MONGODB_PASSWORD
 // $ MONGODB_PASSWORD=<mon_mot_de_passe> node dates.js
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://defaultuser:${PASSWORD}@esgi202003-ljuxa.mongodb.net/test?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(async err => {
+// const uri = `mongodb+srv://defaultuser:${PASSWORD}@esgi202003-ljuxa.mongodb.net/test?retryWrites=true&w=majority`;
+const uri = 'mongodb://localhost:27017'
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+(async () => {
+  await client.connect();
   const collection = client.db("test").collection("dates");
   // affiche la liste des documents de la collection dates dans la sortie standard
   const dates = await collection.find({}).toArray();
   console.log('dates:', dates)
-  client.close();
-});
+  await client.close();
+})();
