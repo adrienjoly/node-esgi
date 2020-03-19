7f46918b899c4d9194ca1240c8e5f95507e3d76d
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-19 10:20:43 +0100

    allow caller to provide MONGODB_URI env var

--- dates.js
+++ dates.js
@@ -1,12 +1,10 @@
-const PASSWORD = process.env.MONGODB_PASSWORD
-// il ne faut jamais stocker de mots de passe dans un dépot git, et encore moins dans un dépot git public !
-// => pour lancer ce script avec votre mot de passe, taper la commande suivante:
-// $ MONGODB_PASSWORD=<mon_mot_de_passe> node dates.js
+// => pour lancer ce script avec serveur mongodb, taper la commande suivante:
+// $ MONGODB_URI=<mon_mot_de_passe> node dates.js
+// # e.g. MONGODB_URI=mongodb+srv://defaultuser:${PASSWORD}@esgi202003-ljuxa.mongodb.net/test?retryWrites=true&w=majority
 
 const MongoClient = require('mongodb').MongoClient;
-// const uri = `mongodb+srv://defaultuser:${PASSWORD}@esgi202003-ljuxa.mongodb.net/test?retryWrites=true&w=majority`;
-const uri = 'mongodb://localhost:27017'
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 (async () => {
   await client.connect();
